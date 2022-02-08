d30dd0799eb82e6e1a30a85d1e3da4441027eedc
Prateek Sharma <ps.prateek.sharma143@gmail.com>
2022-02-09 00:18:26 +0530

    I don't know

--- themes/my_catppuccin/theme.lua
+++ themes/my_catppuccin/theme.lua
@@ -158,7 +158,7 @@ theme.awesome_icon = theme_assets.awesome_icon(
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = "/usr/share/icons/Papirus-Dark"
+theme.icon_theme = 'Papirus-Dark'
 
 -- Set different colors for urgent notifications.
 rnotification.connect_signal('request::rules', function()
